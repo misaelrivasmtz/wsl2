0eaac5e5be591b2468e027e5ad5dea59eb5f194c
misaelrivasmtz <00365520@uca.edu.sv>
2021-10-23 12:08:36 -0600

    23/10

---
+++ ListaCircular.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+
+using namespace std;
+
+struct node{
+    int data;
+    node *next;
+};
+
+class CircularList{
+    private:
+    node *head;
+
+    public:
+    CircularList();
+    ~CircularList();
+    void addFront(int);
+    void addBack(int);
+    void addAscendingOrder(int);
+    void addNodeFront();
+    void addNodeBack();
+    void addNodeAsc();
+    void printForward();
+    void deleteCircularListRec(node *);
+
+};
+
+CircularList::CircularList(){
+    head = NULL;
+}
+
+CircularList::~CircularList(){
+    cout << "Destructor ejecutándose" << endl;
+    if(head){
+        deleteCircularListRec(head);
+        head = NULL;
+    }
+}
+
+void CircularList::deleteCircularListRec(node *h){
+    if(h->next != head)
+        deleteCircularListRec(h->next);
+    delete h;
+}
+
+void CircularList::addFront(int data){
+    node *nd;
+    nd = new node;
+    nd->data = data;
+    if(!head){
+        head = nd;
+        head->next = head;
+    }
+    else{
+        nd->next = head;
+        node *aux = head;
+        while(aux->next != head){
+            aux = aux->next;
+        }
+        aux->next = nd;
+        head = nd;
+    }
+}
+
+void CircularList::addNodeFront(){
+    int data;
+    char opc;
+    cout << "Agregar dato (s/n): ";
+    cin >> opc; cin.ignore();
+    while(opc != 'n' && opc != 'N'){
+        cout << "Digite un numero: ";
+        cin >> data; cin.ignore();
+        addFront(data);
+        cout << "Agregar dato (s/n): ";
+        cin >> opc; cin.ignore();       
+    }
+}
+
+void CircularList::printForward(){
+    node *aux;
+    if(head){
+        aux = head;
+        do{
+            cout << aux->data << endl;
+            aux = aux->next;
+        }while(aux != head);
+    }
+}
+
+int main(void){
+    CircularList objCL;
+
+    cout << "Insertando datos al inicio" << endl;
+    objCL.addNodeFront();
+    
+    cout << "Mostrando elementos en orden directo" << endl;
+    objCL.printForward();
+
+    
+
+
+    return 0;
+}
