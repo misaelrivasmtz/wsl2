71ef4e7e67ad75ba6f736772ce966cf9b0f8a58d
misaelrivasmtz <00365520@uca.edu.sv>
2021-10-23 21:30:37 -0600

    Update

--- ListaCircular.cpp
+++ ListaCircular.cpp
@@ -76,6 +76,78 @@ void CircularList::addNodeFront(){
     }
 }
 
+void CircularList::addBack(int data){
+    node *nd, *aux;
+    nd = new node;
+    nd->data = data;
+    if(!head){
+        head = nd;
+        head->next = head;
+    }
+    else{
+        nd->next = head;
+        aux = head;
+        while(aux->next != head)
+            aux = aux->next;
+        aux->next = nd;
+    }
+}
+
+void CircularList::addNodeBack(){
+    int data;
+    char opc;
+    cout << "Agregar dato (s/n): ";
+    cin >> opc; cin.ignore();
+    while(opc != 'n' && opc != 'N'){
+        cout << "Digite un numero: ";
+        cin >> data; cin.ignore();
+        addBack(data);
+        cout << "Agregar dato (s/n): ";
+        cin >> opc; cin.ignore();       
+    }
+}
+
+void CircularList::addAscendingOrder(int data){
+    node *nd, *aux;
+    nd = new node;
+    nd->data = data;
+    if(!head){
+        head = nd;
+        head->next = head;
+    }
+    else{
+        if(data < head->data){ // si el dato es menor al primero de la lista, se inserta al inicio
+            nd->next = head;
+            aux = head;
+            while(aux->next != head)
+                aux = aux->next;
+            aux->next = nd;
+            head = nd;
+        }
+        else{ //si el dato es mayor que todos los de la lista
+            aux = head;
+            while(aux->next != head && aux->next->data < data)
+                aux = aux->next;
+            nd->next = aux->next;
+            aux->next = nd;
+        }
+    }
+}
+
+void CircularList::addNodeAsc(){
+    int data;
+    char opc;
+    cout << "Agregar dato (s/n): ";
+    cin >> opc; cin.ignore();
+    while(opc != 'n' && opc != 'N'){
+        cout << "Digite un numero: ";
+        cin >> data; cin.ignore();
+        addAscendingOrder(data);
+        cout << "Agregar dato (s/n): ";
+        cin >> opc; cin.ignore();       
+    }
+}
+
 void CircularList::printForward(){
     node *aux;
     if(head){
@@ -90,14 +162,17 @@ void CircularList::printForward(){
 int main(void){
     CircularList objCL;
 
-    cout << "Insertando datos al inicio" << endl;
-    objCL.addNodeFront();
+    //cout << "Insertando datos al inicio" << endl;
+    //objCL.addNodeFront();
     
+    //cout << "Insertando datos al final" << endl;
+    //objCL.addNodeBack();
+
+    cout << "Insertando datos en orden ascendente" << endl;
+    objCL.addNodeAsc();
+
     cout << "Mostrando elementos en orden directo" << endl;
     objCL.printForward();
-
-    
-
 
     return 0;
 }
